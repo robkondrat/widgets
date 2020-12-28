d1d8c7df420c2ee17470491999ff56f2eb49eb04
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-12-28 16:04:33 -0600

    dropdown component started

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import Accordion from './components/Accordion';
-import Search from './components/Search';
+// import Accordion from './components/Accordion';
+// import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
-const items = [
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a front end javascript framework'
+//   },
+//   {
+//     title: 'Why use React?',
+//     content: 'Its a favorite'
+//   },
+//   {
+//     title: 'Use?',
+//     content: 'You use it.'
+//   }
+// ]
+
+const options = [
   {
-    title: 'What is React?',
-    content: 'React is a front end javascript framework'
+    label: 'The Color Red',
+    value: 'red'
   },
   {
-    title: 'Why use React?',
-    content: 'Its a favorite'
+    label: 'The Color Green',
+    value: 'green'
   },
   {
-    title: 'Use?',
-    content: 'You use it.'
+    label: 'Shade of Blue',
+    value: 'blue'
   }
 ]
 
 export default () => {
   return (
     <div>
-      <Search />
+      <Dropdown options={options} />
     </div>
   );
 };
