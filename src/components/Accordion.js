f6487b41c549e74271d376166565a7862d070c8d
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-12-28 13:18:43 -0600

    controlling active with hooks

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
-  return <h1>{items.length}</h1>;
+  const [activeIndex, setActiveIndex] = useState(null);
+  
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  }
+
+  const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : '';
+
+    return (
+      <div key={item.title}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
+          <i className="dropdown icon">
+            {item.title}
+          </i>
+        </div>
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </div>
+    )
+  });
+
+  return <div className="ui styled accordion">
+    {renderedItems}
+  </div>
+  
 };
 
 export default Accordion;
